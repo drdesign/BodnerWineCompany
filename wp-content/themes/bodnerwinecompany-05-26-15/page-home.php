a27a2f116f0590d83949017b5926e04f6d1133ab
Danniel Rolfe <info@dannielrolfe.com>
2015-06-02 14:37:54 -0700

    removed and added plugins

--- wp-content/themes/bodnerwinecompany-05-26-15/page-home.php
+++ wp-content/themes/bodnerwinecompany-05-26-15/page-home.php
@@ -1,18 +1,22 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
+ 	Template Name: Home Page
  * @package bodnerWineCompany-05-26-15
  */
 
 get_header(); ?>
 	</header>
+		<div class="slider-border"></div> 
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				
+					<div id="slides" class="slider">
+					    <img src="" class="slider-img-1">
+					    <img src="http://placehold.it/940x528" class="slider-img-2">
+					    <img src="http://placehold.it/940x528" class="slider-img-3">
+					    <img src="http://placehold.it/940x528" class="slider-img-4">
+					 
+				</div>
 			<?php if ( have_posts() ) : ?>
 
 				<?php /* Start the Loop */ ?>
